827b465cbe3a8b3ca27b7119039ed506ed740b8e
Biplav Raj Raut <biplavraut@gmail.com>
2021-03-24 16:30:06 +0545

    Update bottombanner.blade.php

--- resources/views/frontend/includes/bottombanner.blade.php
+++ resources/views/frontend/includes/bottombanner.blade.php
@@ -6,8 +6,8 @@
             </div>
             <div class="col-12 col-xl-6 col-lg-7 wow animate__fadeIn">
                 
-                <h1 class="alt-font text-white font-weight-500 margin-40px-bottom">Enhance Your <br>Lifestyle</h1>
-                <div class="margin-25px-bottom text-white opacity-6 alt-font">SHOP SMART, LIVE EASY.</div>
+                <h1 class="alt-font text-white font-weight-900 margin-15px-bottom" style="letter-spacing: 0.5px;">Enhance Your <br>Lifestyle</h1>
+                <div class="margin-25px-bottom text-white opacity-6 alt-font" style="letter-spacing: 2.5px; font-size: 22px;">SHOP SMART, LIVE EASY.</div>
                 <a href="mailto:info@yourdomain.com" class="btn btn-large btn-expand-ltr text-white lg-margin-15px-bottom md-no-margin-bottom md-margin-auto-lr">info@ozeroneshop.com<span class="bg-transparent-black"></span></a>
             </div>
         </div>
